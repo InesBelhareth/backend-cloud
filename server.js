f2a14ed89f67ef65d08215576aef0a09b7c37e8a
InesBelhareth <inesbelhareth4@gmail.com>
2026-01-15 09:21:27 +0100

    fix Port

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.delete('/api/submissions/:id', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
